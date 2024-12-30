61164b51b3ebcac240f89f0b8d238d9eeaf6f390
SimoneCheng <bleach1827@gmail.com>
2024-12-30 23:33:33 +0800

    feat: show router devTool only at development

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,6 +1,8 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 
+const isDevelopment = import.meta.env.DEV;
+
 export const Route = createRootRoute({
   component: RootComponent,
 });
@@ -9,7 +11,7 @@ function RootComponent() {
   return (
     <>
       <Outlet />
-      <TanStackRouterDevtools position="bottom-right" />
+      {isDevelopment && <TanStackRouterDevtools position="bottom-right" />}
     </>
   )
 }
